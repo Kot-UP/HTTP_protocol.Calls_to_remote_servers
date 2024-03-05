0b23c9eacd27ca7c3a1050e32062b693a506a820
Perminov A.O. <Triela1991@mail.ru>
2024-03-05 11:32:03 +0700

    Initial commit

---
+++ src/main/java/org/example/task_2/Main.java
@@ -0,0 +1,43 @@
+package org.example.task_2;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.example.task_1.Post;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static final String NASA = "https://api.nasa.gov/planetary/apod?api_key=2o2mrv2IPKeKXDqLtSigRh2KOizscZvbbYhcd10I";
+    public static final ObjectMapper mapper = new ObjectMapper();
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        // создаем объект запроса с произвольными заголовками
+        HttpGet request = new HttpGet(NASA);
+        //request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        // отправка запроса
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        List<Post> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Post>>() {});
+        posts.forEach(System.out::print);
+
+    }
+}
